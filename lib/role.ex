a58dd755478013b02bb8184cfb3f78e5ae3a4bb6
Ivan Kozik <ivan@ludios.org>
2017-11-24 06:40:52 +0000

    Install firmware-iwlwifi

--- lib/role.ex
+++ lib/role.ex
@@ -1,6 +1,7 @@
 defmodule RoleWifiClient do
 	def role(_tags \\ []) do
 		%{
+			desired_packages: ["firmware-iwlwifi"],
 			# Needed for wifi to work, at least on an HP 8460p running xenial
 			# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=749201
 			boot_time_kernel_modules: ["ccm", "ctr"]
