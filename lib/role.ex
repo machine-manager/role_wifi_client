4815a5a491759d458f0340b63fbc531ab3d87a74
Ivan Kozik <ivan@ludios.org>
2017-12-20 06:23:03 +0000

    Install cmst (Qt UI for connman) and wpasupplicant

--- lib/role.ex
+++ lib/role.ex
@@ -1,7 +1,9 @@
 defmodule RoleWifiClient do
 	def role(_tags \\ []) do
 		%{
-			desired_packages: ["firmware-iwlwifi"],
+			# cmst/connman can actually bring up wireless networking properly
+			# unlike all the other junk out there
+			desired_packages: ["firmware-iwlwifi", "cmst", "wpasupplicant"],
 			# Needed for wifi to work, at least on an HP 8460p running xenial
 			# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=749201
 			boot_time_kernel_modules: ["ccm", "ctr"]
